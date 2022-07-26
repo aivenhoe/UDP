99c1fe955f2ac43ae15decc10219b8487e048003
ivo <ivo.schuessler@iart.ch>
2022-07-26 15:12:18 +0200

    general cleanup, udp receiving is now done via subscription

---
+++ UDP/UdpReceiver.cs
@@ -0,0 +1,113 @@
+﻿//Inspired by this thread: https://forum.unity.com/threads/simple-udp-implementation-send-read-via-mono-c.15900/
+//thanks OP la1n
+//thanks MattijsKneppers for letting me know that I also need to lock my queue while enqueuing
+//adapted during projects according to my needs
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+public enum UDPReceiveMode { Discard, Queue };
+
+/// <summary>
+/// General purpose UDP receiver class. see also: <see cref="UdpSender"/>
+/// </summary>
+public class UdpReceiver
+{
+    public int Port { get; private set; }
+
+    public UDPReceiveMode Mode {get; private set;}
+    public bool ReceiveThreadIsRunning { get; private set; }
+
+    public delegate void ReceivedData(byte[] data);
+    public event ReceivedData ReceivedNewData;
+
+    public byte[] LastReceivedUDPPacket = new byte[0];
+    public DateTime LastReceivedPacketTimestamp;
+
+    public int QueuedPacketsCount { get; private set; }
+    private readonly Queue<byte[]> _queueOfReceivedUDPpackets = new Queue<byte[]>();
+
+
+    private UdpClient _client;
+    private Thread _receiveThread;
+
+    //Since receiving is running in a separte thread at maximum speed, incomming messages are queued up, so they can be analyzed in the main update loop.
+    public UdpReceiver(int localPort, UDPReceiveMode mode)
+    {     
+        Port = localPort; 
+        Mode = mode;
+        QueuedPacketsCount = -1;
+
+        Debug.Log($"Listening on UDP port: {Port} with {nameof(UDPReceiveMode)} {Mode}");
+
+        ReceiveThreadIsRunning = true;
+        _receiveThread = new Thread(new ThreadStart(ReceiveData));
+        _receiveThread.IsBackground = true;
+        _receiveThread.Start();
+
+    }
+
+    private void ReceiveData()
+    {
+        _client = new UdpClient(Port);
+        while (ReceiveThreadIsRunning)
+        {
+            try
+            {             
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = _client.Receive(ref anyIP);
+                if (data.Length > 0)
+                {
+                    LastReceivedUDPPacket = data.ToArray();      
+
+                    LastReceivedPacketTimestamp = DateTime.Now;
+
+                    ReceivedNewData.Invoke(LastReceivedUDPPacket);
+
+                    //Debug.Log("ReceivedUDP Data >> " + lastReceivedUDPPacket + " with timestamp: " + timestamp);
+
+                    if (Mode == UDPReceiveMode.Queue)
+                    {
+                        lock (_queueOfReceivedUDPpackets)
+                        {
+                            _queueOfReceivedUDPpackets.Enqueue(LastReceivedUDPPacket);
+                            QueuedPacketsCount = _queueOfReceivedUDPpackets.Count;
+                        }
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                Debug.Log(err.ToString());      
+            }
+        }
+    }
+    public Queue<byte[]> GetQueuedUDPPackets() 
+    {
+      lock(_queueOfReceivedUDPpackets)
+        {
+            Queue<byte[]> packetQueue = new Queue<byte[]>(_queueOfReceivedUDPpackets);
+            _queueOfReceivedUDPpackets.Clear();
+            return packetQueue;
+        }
+    }
+    public void CloseReceving()
+    {
+        Debug.Log($"closing receiving UDP on port: {Port}"); 
+
+        if (_receiveThread != null)
+            _receiveThread.Abort();
+
+        _client.Close();
+        ReceiveThreadIsRunning = false;
+    }
+    ~UdpReceiver()
+    {
+        CloseReceving();
+    }
+}
---
+++ UDP/UdpSender.cs
@@ -0,0 +1,112 @@
+﻿//Inspired by this thread: https://forum.unity.com/threads/simple-udp-implementation-send-read-via-mono-c.15900/
+//thanks OP la1n
+//thanks MattijsKneppers for letting me know that I also need to lock my queue while enqueuing
+//adapted during projects according to my needs
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// General purpose UDP receiver class. see also: <see cref="UdpReceiver"/>
+/// </summary>
+public class UdpSender 
+{
+    public string IpAddress { get; private set; }
+    public int SourcePort { get; private set; } //sometimes we need to define the source port, since some devices only accept messages coming from a predefined sourceport.
+    public int RemotePort { get; private set; }
+
+    private IPEndPoint _remoteEndPoint;
+    private UdpClient _client;
+
+    public UdpSender(string ipAddress, int remotePort, int sourcePort = -1) //if sourceport is not set, its being chosen randomly by the system
+    {
+        IpAddress = ipAddress;
+        SourcePort = sourcePort;
+        RemotePort = remotePort;
+
+        _remoteEndPoint = new IPEndPoint(IPAddress.Parse(IpAddress), RemotePort);
+        if (this.SourcePort <= -1)
+        {
+            _client = new UdpClient();
+            Debug.Log($"Sending to {IpAddress}:{RemotePort}");
+        }
+        else
+        {
+            _client = new UdpClient(this.SourcePort);
+            Debug.Log($"Sending to {IpAddress}:{RemotePort} from Source Port:{SourcePort}");
+        }
+
+    }
+
+    ~UdpSender()
+    {
+        _client.Close();
+    }
+    // sendData in different ways. Can be extended accordingly
+    public void SendData(byte[] data)
+    {
+        try
+        {
+            _client.Send(data, data.Length, _remoteEndPoint);
+        }
+        catch (Exception err)
+        {
+            Debug.Log(err.ToString());
+        }
+    }
+    public void sendString(string message)
+    {
+        try
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            _client.Send(data, data.Length, _remoteEndPoint);
+
+        }
+        catch (Exception err)
+        {
+            Debug.Log(err.ToString());
+        }
+    }
+
+    public void sendInt32(Int32 myInt)
+    {
+        try
+        {  
+            byte[] data = BitConverter.GetBytes(myInt);
+            _client.Send(data, data.Length, _remoteEndPoint);
+        }
+        catch (Exception err)
+        {
+            Debug.Log(err.ToString());
+        }
+    }
+    public void sendInt32Array(Int32[] myInts)
+    {
+        try
+        {
+            byte[] data = new byte[myInts.Length * sizeof(Int32)];
+            Buffer.BlockCopy(myInts, 0, data, 0, data.Length);
+            _client.Send(data, data.Length, _remoteEndPoint);
+        }
+        catch (Exception err)
+        {
+            Debug.Log(err.ToString());
+        }
+    }
+    public void sendInt16Array(Int16[] myInts)
+    {
+        try
+        {
+            byte[] data = new byte[myInts.Length * sizeof(Int16)];
+            Buffer.BlockCopy(myInts, 0, data, 0, data.Length);
+            _client.Send(data, data.Length, _remoteEndPoint);
+        }
+        catch (Exception err)
+        {
+            Debug.Log(err.ToString());
+        }
+    }
+}
